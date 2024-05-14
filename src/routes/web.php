8f138143075548e7b0b99e4b7929073e0f8dc380
mizan <mizanur090148>
2024-05-14 18:34:10 +0600

    env and others docker compose file

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    // DB::table('users')->insert([
+    //     'name' => 'Mr milon',
+    //     'email' => 'milon02@gmail.com', // Corrected the typo, added comma after 'email'
+    //     'password' => '1234567'
+    // ]);
     return view('welcome');
 });
