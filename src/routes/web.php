6e88c20b061ec20e7feccf44d7007f4abd68bda5
mizanur090148 <mizanur090148@gmail.com>
2025-07-19 23:49:01 +0600

    updated docker compose yaml file

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // DB::table('users')->insert([
-    //     'name' => 'Mr milon',
-    //     'email' => 'milon02@gmail.com', // Corrected the typo, added comma after 'email'
-    //     'password' => '1234567'
-    // ]);
+    DB::table('tests')->insert([
+        'name' => 'Mr milon'
+    ]);
     return view('welcome');
 });
